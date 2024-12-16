77b65234b525776c8d1e09bbbcf2f780d3e98c9b
Fayiz kv <fayizkvcklm2002@gmail.com>
2024-12-16 12:01:20 +0530

    signup implemented

---
+++ models/usermodel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    match: [/^\d{10,15}$/, 'Please use a valid phone number'], //phone validation
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const dbURI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  // Check if already connected
+  if (mongoose.connection.readyState === 1) {
+    console.log('Already connected to MongoDB');
+    return;
+  }
+
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log('Failed to connect to MongoDB', err);
+  }
+};
+
+module.exports = connectDB;
