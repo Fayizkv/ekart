76923ad4eab937818bfb3f08387a65a99599dc86
Fayiz kv <fayizkvcklm2002@gmail.com>
2024-12-17 13:27:57 +0530

    admin userpage basics implemented

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 var product = require('../models/productmodel');
+var User = require('../models/usermodel');
 var connectDB = require('./mongo')
 
 //admin login
 
 //admin logout
 
-//admin page
-router.get('/', async (req,res)=>{
+//admin product page
+router.get('/products', async (req,res)=>{
 
     await connectDB();
 
     var products = await product.find();
 
-    res.render('admin', { products });
+    res.render('adminproducts', { products });
 })
+
+//admin user page
+router.get('/users', async(req,res)=>{
+
+    await connectDB();
+
+    var users = await User.find();
+    console.log(users);
+    res.render('adminusers', {users});
+});
+
 
 module.exports = router;
